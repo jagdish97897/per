2a691130701ca8d105dd170c10b4c5703801e466
jagdish <jagdish0000singh@gmail.com>
2023-08-24 23:07:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [data, setName] = useState()
+  function getData(e) {
+    // console.log(val.target.value)
+    setName(e.target.value)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+
+      {/* <h1>{data}</h1> */}
+      <h1>bihar board result</h1>
+      <h3>enter yor percentage:<input type="text" onChange={getData}></input></h3>
+      
+      {
+        (data >= 33) ?
+          <h1>pass</h1>
+          :
+          <h1>fail</h1>
+
+      }
+
+    </>
+  )
+
+
 }
 
 export default App;
